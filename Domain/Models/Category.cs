6ea7e57453f1010195aa9851c8d5ce3bfb78fd03
PavloSokol <34427149+PavloSokol@users.noreply.github.com>
2019-11-13 21:35:35 +0200

    Add files via upload

---
+++ Domain/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Domain.Models
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public IList<Product> Products { set; get; } = new List<Product>();
+    }
+}
